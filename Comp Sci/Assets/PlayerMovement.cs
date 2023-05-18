409d35dd64729234fd487c277a9c109643370f0e
tomPers1073 <tomcpaul1234@gmail.com>
2023-05-18 11:39:47 -0400

    can move on floor now

--- Comp Sci/Assets/PlayerMovement.cs
+++ Comp Sci/Assets/PlayerMovement.cs
@@ -4,8 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f; //Controls velocity multiplier
 
-    Rigidbody rb; //Tells script there is a rigidbody, we can use variable rb to reference it in further script
-
+    Rigidbody rb; //Tells script there is a rigidbody, we can use variable rb to reference it in further script
+    public float mult = 4;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +17,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         float xMove = Input.GetAxisRaw("Horizontal"); // d key changes value to 1, a key changes value to -1
         float zMove = Input.GetAxisRaw("Vertical"); // w key changes value to 1, s key changes value to -1
         //float YMove = Input.GetKeyDown(KeyCode.Space); // w key changes value to 1, s key changes value to -1
         if(Input.GetKeyDown("space")){
-            rb.velocity = new Vector3(-xMove * 2, 6, -zMove * 2) * speed;
+            rb.velocity = new Vector3(-xMove * mult, 6, -zMove * mult) * speed;
         }
         else
         {
-            rb.velocity = new Vector3(-xMove * 2, rb.velocity.y, -zMove * 2) * speed;
+            rb.velocity = new Vector3(-xMove * mult, rb.velocity.y, -zMove * mult) * speed;
         }
          // Creates velocity in direction of value equal to keypress (WASD). rb.velocity.y deals with falling + jumping by setting velocity to y. 
     }
